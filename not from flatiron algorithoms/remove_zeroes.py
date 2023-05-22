9cdb2db8f2cbe4f6ad2802950ba5e06be1e542db
Eli Moshe <moshebogo@gmail.com>
2023-05-22 06:56:25 -0400

    remove_zeroes python

---
+++ not from flatiron algorithoms/remove_zeroes.py
@@ -0,0 +1,13 @@
+heights   = [9, 0, 0, 6, 0, 3, 0, 5]
+# result  = [3, 0, 0, 5, 0, 6, 0, 9]
+
+class Solution:
+    def remove_zeroes(array):
+        return array
+
+
+
+if __name__ == '__main__':
+    s = Solution()
+    print(s.remove_zeroes(heights))
+
