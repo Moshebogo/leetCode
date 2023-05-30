d3d0077c335b2446f592d153a455d9657d968166
Eli Moshe <moshebogo@gmail.com>
2023-05-29 22:31:28 -0400

    stuff

---
+++ not from flatiron algorithoms/practice.js
@@ -0,0 +1,8 @@
+function findEvenNum(num){
+   return num % 2 === 0 ? `${num} is an even  number, good job.` : "the number is odd, sorry." 
+}
+
+console.log(findEvenNum(1))
+
+
+console.log(findEvenNum(2))
--- not from flatiron algorithoms/remove_zeroes.py
+++ not from flatiron algorithoms/remove_zeroes.py
@@ -1,13 +1,26 @@
 heights   = [9, 0, 0, 6, 0, 3, 0, 5]
 # result  = [3, 0, 0, 5, 0, 6, 0, 9]
 
+def indexed(array_2):
+   array = array_2
+   indexed = []
+   for num in array:
+       if num == 0:
+           indexed.append(array.index(num))
+           array.pop(num)
+           print("array =>", array)
+   return indexed       
+
+
 class Solution:
-    def remove_zeroes(array):
-        return array
+    def remove_zeroes(self, array):
+       pass
+        
 
 
 
 if __name__ == '__main__':
     s = Solution()
-    print(s.remove_zeroes(heights))
+    # print(s.remove_zeroes(heights))
+    print(indexed(heights))
 
