eabbf582545c7a5a11b3036a9c38d1d1438ff635
Eli Moshe <moshebogo@gmail.com>
2023-07-04 10:45:32 -0400

    remove_zeroes completedgit status!

--- not from flatiron algorithoms/remove_zeroes.py
+++ not from flatiron algorithoms/remove_zeroes.py
@@ -1,26 +1,32 @@
-heights   = [9, 0, 0, 6, 0, 3, 0, 5]
+array     = [9, 0, 0, 6, 0, 3, 0, 5]
 # result  = [3, 0, 0, 5, 0, 6, 0, 9]
-
-def indexed(array_2):
-   array = array_2
-   indexed = []
-   for num in array:
-       if num == 0:
-           indexed.append(array.index(num))
-           array.pop(num)
-           print("array =>", array)
-   return indexed       
-
 
 class Solution:
     def remove_zeroes(self, array):
-       pass
+
+        final_list = []
+
+        temp = []
+        for num in array:
+            temp.append(num) if num != 0 else None 
+        no_zeroes = sorted(temp)
         
+        for i, v in enumerate(array):
+            if v == 0:
+                final_list.append(array[i])
+            else:
+                final_list.append(no_zeroes[0])
+                del no_zeroes[0]
+
+        return (final_list) 
+        
+
+
+
+
 
 
 
 if __name__ == '__main__':
     s = Solution()
-    # print(s.remove_zeroes(heights))
-    print(indexed(heights))
-
+    print((s.remove_zeroes(array)))
